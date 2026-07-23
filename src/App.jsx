0ffa4074d94d86b6b35ac36ce1aa93d7db9ab6a8
ssm0123 <shinyho01@naver.com>
2026-07-23 20:36:05 +0900

    홈페이지(소개페이지) 제작
    헤더에 mywheel 누르면 뜨는 홈페이지 (서비스 소개 페이지) 완성 휠타이어제원 아직 개발 안돼서 지금은 map에 연결되어있음 차후 수정필요

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-// src/App.jsx 예시 (리액트 라우터 세팅 가정)
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'; // 방금 만든 Header 컴포넌트 import
-// import Home from './pages/Home'; // Home 페이지 (예시)
-// import MapPage from './pages/MapPage'; // Map 페이지 (예시)
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* 모든 페이지 상단에 고정으로 렌더링됨 */}
-        <Header />
-
-        {/* 페이지별 화면 단위 컴포넌트 배치 */}
-        <main style={{ padding: '2rem' }}>
-          {/* <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/map" element={<MapPage />} />
-            {/* 다른 페이지 라우팅 추가 }
-          </Routes> */}
-        </main>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* 추후에 완성될 다른 페이지들 */}
+        {/* <Route path="/custom" element={<CustomPage />} /> */}
+        {/* <Route path="/map" element={<MapPage />} /> */}
+        {/* <Route path="/community" element={<CommunityPage />} /> */}
+      </Routes>
     </Router>
   );
 }
