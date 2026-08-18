c0c1e7836790c3cc278e59da30bcbed65c8cd81b
rudtnsiasia <leeks1106@naver.com>
2026-08-19 04:10:36 +0900

    map이랑 회원가입 로그인 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 
 import MyPage from './pages/MyPage';
+import MapPage from './pages/MapPage';
 import PostList from './PostList';
 import PostCreate from './PostCreate';
 import PostDetail from './PostDetail';
@@ -16,9 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/map" element={<MapPage />} />
         {/* 추후에 완성될 다른 페이지들 */}
         {/* <Route path="/custom" element={<CustomPage />} /> */}
-        {/* <Route path="/map" element={<MapPage />} /> */}
         {/* <Route path="/community" element={<CommunityPage />} /> */}
         {/* 커뮤니티 및 글작성 페이지 연결 */}
         <Route path="/community" element={<PostList />} />
